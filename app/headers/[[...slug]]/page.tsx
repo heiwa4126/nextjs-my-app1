9212d94693bea7907eb8ee8b12cc523f82ac16a1
heiwa4126 <heiwa4126@gmail.com>
2023-08-01 10:59:18 +0900

    update

--- app/headers/[[...slug]]/page.tsx
+++ app/headers/[[...slug]]/page.tsx
@@ -1,21 +1,28 @@
 import { Metadata } from 'next';
 import { headers } from 'next/headers';
+import Link from 'next/link';
+
 const title = 'headers';
 export const metadata: Metadata = {
   title
 };
 
-export default async function Page({
-  params,
-  searchParams
-}: {
+interface PageProps {
   params: { slug: string };
   searchParams: { [key: string]: string | string[] | undefined };
-}) {
+}
+
+export default async function Page({ params, searchParams }: PageProps) {
   const headersList = headers();
   return (
     <main className="mx-4 my-3">
-      {/* <h1>{title}</h1>
+      <h1>{title}</h1>
+      <p>
+        See:{' '}
+        <Link href="https://nextjs.org/docs/app/api-reference/file-conventions/page">
+          File Conventions: page.js | Next.js
+        </Link>
+      </p>
       <ul>
         <li>
           <Link href="/headers?a=1&b=2">{'/headers?a=1&b=2'}</Link>
@@ -26,7 +33,7 @@ export default async function Page({
         <li>
           <Link href="/headers/test/foo/bar?a=5&b=6">{'/headers/test/foo/bar?a=5&b=6'}</Link>
         </li>
-      </ul> */}
+      </ul>
       <h2>searchParams & params</h2>
       <pre>{JSON.stringify({ searchParams, params }, null, 2)}</pre>
       <h2>headers</h2>
