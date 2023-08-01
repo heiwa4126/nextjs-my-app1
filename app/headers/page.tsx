7d6e2fa1fc6d8fbe9e7deec645bc690011de65bf
heiwa4126 <heiwa4126@gmail.com>
2023-08-01 10:50:53 +0900

    fix wrong args in app/headers/page.tsx

--- app/headers/page.tsx
+++ app/headers/page.tsx
@@ -1,24 +1,34 @@
 import { Metadata } from 'next';
 import { headers } from 'next/headers';
-import Link from 'next/link';
-
 const title = 'headers';
 export const metadata: Metadata = {
   title
 };
 
-export default async function Page(request: Request) {
+export default async function Page({
+  params,
+  searchParams
+}: {
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
   const headersList = headers();
   return (
     <main className="mx-4 my-3">
-      <h1>{title}</h1>
+      {/* <h1>{title}</h1>
       <ul>
         <li>
           <Link href="/headers?a=1&b=2">{'/headers?a=1&b=2'}</Link>
         </li>
-      </ul>
-      <h2>request</h2>
-      <pre>{JSON.stringify(request, null, 2)}</pre>
+        <li>
+          <Link href="/headers/test?a=3&b=4">{'/headers/test?a=3&b=4'}</Link>
+        </li>
+        <li>
+          <Link href="/headers/test/foo/bar?a=5&b=6">{'/headers/test/foo/bar?a=5&b=6'}</Link>
+        </li>
+      </ul> */}
+      <h2>searchParams & params</h2>
+      <pre>{JSON.stringify({ searchParams, params }, null, 2)}</pre>
       <h2>headers</h2>
       <pre>{JSON.stringify(headersList, null, 2)}</pre>
     </main>
