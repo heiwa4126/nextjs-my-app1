d5b65d0f1ec089053aee0f6a259882a7cdcbd78d
heiwa4126 <heiwa4126@gmail.com>
2023-08-01 18:03:36 +0900

    (backup)

--- app/headers/[[...slug]]/page.tsx
+++ app/headers/[[...slug]]/page.tsx
@@ -12,6 +12,8 @@ interface PageProps {
   searchParams: { [key: string]: string | string[] | undefined };
 }
 
+const links = ['/headers?a=1&b=2', '/headers/test?a=3&b=4', '/headers/test/foo/bar?a=5&b=6'];
+
 export default async function Page({ params, searchParams }: PageProps) {
   const headersList = headers();
   return (
@@ -24,15 +26,13 @@ export default async function Page({ params, searchParams }: PageProps) {
         </Link>
       </p>
       <ul>
-        <li>
-          <Link href="/headers?a=1&b=2">{'/headers?a=1&b=2'}</Link>
-        </li>
-        <li>
-          <Link href="/headers/test?a=3&b=4">{'/headers/test?a=3&b=4'}</Link>
-        </li>
-        <li>
-          <Link href="/headers/test/foo/bar?a=5&b=6">{'/headers/test/foo/bar?a=5&b=6'}</Link>
-        </li>
+        {links.map((link: string) => {
+          return (
+            <li key={link} className="m-0 p-0">
+              <Link href={link}>{link}</Link>
+            </li>
+          );
+        })}
       </ul>
       <h2>searchParams & params</h2>
       <pre>{JSON.stringify({ searchParams, params }, null, 2)}</pre>
