023f1530ffb1bd28f759c5a2c454816e889f18e0
heiwa4126 <heiwa4126@gmail.com>
2023-08-03 17:06:46 +0900

    update

--- lib/global.ts
+++ lib/global.ts
@@ -1,5 +1,13 @@
 import { basePath } from '@/next.config';
 
 const rawSelfUrl = process.env.NEXT_SELFURL ?? 'http://localhost:3000';
+
+/** 環境変数 `NEXT_SELFURL`+`NEXTCONFIG_BASEPATH` NEXT_SELFURLのデフォルト値は http://localhost:3000 。
+セルフホストしているAPIに対してreact server componentからアクセスするのに使う。
+ */
 export const selfUrl = rawSelfUrl + basePath;
+
+/** 環境変数 `NEXT_BASEURL`+`NEXTCONFIG_BASEPATH` NEXT_BASEURLのデフォルト値は NEXT_SELFURL 。
+セルフホストしているAPIに対してreact client componentからアクセスするのに使う。
+ */
 export const baseUrl = (process.env.NEXT_BASEURL ?? rawSelfUrl) + basePath;
